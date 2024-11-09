b1f7ca9c6639c5a03830e65a1eb44ba1709db053
Gema <132750075+GemaVNZ@users.noreply.github.com>
2024-11-09 22:03:01 +0100

    Se añaden los diferentes ficheros SQL y se empiezan a realizar las tareas.

---
+++ seeding.sql
@@ -0,0 +1,32 @@
+
+-- Insert data into the CARS table
+
+INSERT INTO CARS VALUES (1,'3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', '2019', 'Azul');
+INSERT INTO CARS VALUES (2,'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', '2019', 'Rojo');
+INSERT INTO CARS VALUES (3,'RKXVNNIHLVVZOUB4M', 'Vado', 'Fusión', '2018', 'Blanco');
+INSERT INTO CARS VALUES (4,'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', '2018', 'Plata');
+INSERT INTO CARS VALUES (5,'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', '2019', 'Gris');
+INSERT INTO CARS VALUES (6,'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Campo Traviesa', '2019', 'Gris');
+
+-- Insert data into the CUSTOMERS table
+
+INSERT INTO CUSTOMERS VALUES (0,'Pablo Picasso', '+34 636 17 63 82', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'España', '28045');
+INSERT INTO CUSTOMERS VALUES (1,'Abraham Lincoln', '+1 305 907 7086', '120 SW Calle 8', 'Miami', 'Florida', 'Estados Unidos', '33130');
+INSERT INTO CUSTOMERS VALUES (2,'Napoleon Bonaparte', '+33 1 79 75 40 00', '40 Calle del Coliseo', 'París', 'Isla de Francia', 'Francia', '75008');
+
+-- Insert data into the SALESPERSONS table
+
+INSERT INTO SALESPERSONS VALUES (1,'00001', 'Petey Cruiser', 'Madrid');
+INSERT INTO SALESPERSONS VALUES (2,'00002', 'Anna Sthesia', 'Barcelona');
+INSERT INTO SALESPERSONS VALUES (3,'00003', 'Pablo Molive', 'Berlin');
+INSERT INTO SALESPERSONS VALUES (4,'00004', 'Gail Viento de Fuerza', 'París');
+INSERT INTO SALESPERSONS VALUES (5,'00005', 'Paige Turner', 'Mimia');
+INSERT INTO SALESPERSONS VALUES (6,'00006', 'Bob Frapples', 'Ciudad de México');
+INSERT INTO SALESPERSONS VALUES (7,'00007', 'Walter Melón', 'Ámsterdam');
+INSERT INTO SALESPERSONS VALUES (8,'00008', 'Shonda Leer', 'San Pablo');
+
+-- Insert data into the INVOICES table
+
+INSERT INTO INVOICES VALUES (1,'852399038', '22-08-2018', 1, 1, 3);
+INSERT INTO INVOICES VALUES (2,'731166526', '31-12-2018', 3, 3, 5);
+INSERT INTO INVOICES VALUES (3,'271135104', '22-01-2019', 2, 2, 7);
